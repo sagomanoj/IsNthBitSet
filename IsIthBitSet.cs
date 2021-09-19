c3e497af511dab51893d61e170d0c2a6e1295f5e
sagomanoj <33486909+sagomanoj@users.noreply.github.com>
2021-09-19 13:18:17 +0530

    Returns true if the Bit at the Nth Position is Set

---
+++ IsIthBitSet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class IsIthBitSet
+    {
+        //Given a number and find if the i the bit is Set
+
+        //Move the number i times right and do & with 1, if 1 then i th bit is set
+        public bool isIthBitSet(int number,int i)
+        {
+           return  ((number >> i) & 1) == 1;
+        }
+
+    }
+}
